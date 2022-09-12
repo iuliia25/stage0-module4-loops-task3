c3a9acf7a38f6390315a9cea5db99074891f5ded
iuliia25 <khlystun.iuliia@gmail.com>
2022-09-12 16:25:56 +0300

    done

--- src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
+++ src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
@@ -6,13 +6,13 @@ public class FibonacciSeries {
         int b = 1;
         System.out.println(a);
         System.out.println(b);
-        int c = 1;
+        int c = a + b;
 
         while (c <= lastFibonacci) {
-            c = a + b;
             a = b;
             b = c;
             System.out.println(c);
+            c = a + b;
         }
     }
 }
