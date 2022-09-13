9fd2615fad60c790a7ad3ba3c68c9b254b502882
iuliia25 <khlystun.iuliia@gmail.com>
2022-09-13 15:51:56 +0300

    done

--- src/main/java/school/mjc/stage0/loops/task3/GreatestCommonDivisor.java
+++ src/main/java/school/mjc/stage0/loops/task3/GreatestCommonDivisor.java
@@ -2,13 +2,21 @@ package school.mjc.stage0.loops.task3;
 
 public class GreatestCommonDivisor {
     public void printGCD(int first, int second) {
-        int i = 1;
-        int divider = 1;
-        while (i > first || i > second) {
-            if (first % i == 0 && second % i ==0) {
-                divider = i;
-            }
-        }
-        System.out.println(divider);
+      int divisor = 1;
+      if (first > second) {
+          for (int i = 1; i <= second; i ++) {
+              if (first / i == 0 && second / i == 0) {
+                  divisor = i;
+              }
+          }
+          System.out.println(divisor);
+      } else {
+          for (int f = 1; f <= first; f ++) {
+              if (first / f == 0 && second / f == 0) {
+                  divisor = f;
+              }
+          }
+          System.out.println(divisor);
+      }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
@@ -2,15 +2,12 @@ package school.mjc.stage0.loops.task3;
 
 public class NinesSum {
     public void calculateSum(int lengthOfLastNumber) {
-        String sum = "";
-        for (int i = 0; i < lengthOfLastNumber; i ++){
-            sum += "9";
+        int sum = 0;
+        int term = 0;
+        for (int i = 0; i <= lengthOfLastNumber; i ++) {
+            term = (term * 10) + 9;
+            sum += term;
         }
-        int lastNumber = Integer.parseInt(sum);
-        int result = 0;
-        for (int s = lastNumber; s >= 9; s = s / 10) {
-            result += s;
-        }
-        System.out.println(result);
+        System.out.println(sum);
     }
 }
